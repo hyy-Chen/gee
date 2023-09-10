8f226c0eee392a3897be332b773145d140b6c2d9
ikkk <1584428403@qq.com>
2023-09-10 15:32:23 +0800

    新增动态路由，通配符路由的设置

--- v1/gee/node.go
+++ v1/gee/node.go
@@ -1,19 +1,53 @@
 /**
  @author : ikkk
  @date   : 2023/9/5
- @text   : nil
+ @text   : 存储前缀树节点的结构体，里面存储路由信息
+	需要解决的是路由匹配优先级和路由冲突问题
+
 **/
 
 package gee
+
+import "strings"
 
 type node struct {
 	part       string           // 当前节点的路由名称
 	children   map[string]*node // 静态路由子节点信息
 	handleFunc HandleFunc       //当前节点处理函数
+	paramChild *node            // 参数路由
+	starChild  *node            // 通配符路由
+
 }
 
 // 插入一个路由节点信息
 func (n *node) addNode(part string) (*node, bool) {
+	if strings.HasPrefix(part, "*") {
+		// 如果要插入一个通配符子节点
+		if n.paramChild != nil {
+			// 如果之前已经有一个参数子节点，就会发生冲突, 插入失败
+			return nil, false
+		}
+		if n.starChild == nil {
+			n.starChild = &node{part: part}
+		}
+		if n.starChild.part != part {
+			return nil, false
+		}
+		return n.starChild, true
+	}
+	if strings.HasPrefix(part, ":") {
+		if n.starChild != nil {
+			// 如果之前已经存在一个通配符子节点，就会发生插入冲突
+			return nil, false
+		}
+		if n.paramChild == nil {
+			n.paramChild = &node{part: part}
+		}
+		if n.paramChild.part != part {
+			return nil, false
+		}
+		return n.paramChild, true
+	}
 	if n.children == nil {
 		n.children = make(map[string]*node)
 	}
@@ -30,8 +64,23 @@ func (n *node) addNode(part string) (*node, bool) {
 // 获取一个路由节点信息
 func (n *node) getNode(part string) *node {
 	if n.children == nil {
+		if n.paramChild != nil {
+			return n.paramChild
+		}
+		if n.starChild != nil {
+			return n.starChild
+		}
 		return nil
 	}
-	child := n.children[part]
+	child, ok := n.children[part]
+	if !ok {
+		if n.paramChild != nil {
+			return n.paramChild
+		}
+		if n.starChild != nil {
+			return n.starChild
+		}
+		return nil
+	}
 	return child
 }
--- v1/gee/router.go
+++ v1/gee/router.go
@@ -7,9 +7,19 @@
 package gee
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"strings"
+)
+
+var (
+	ErrRouterPathEmpty               = errors.New("web: 路由路径为空")
+	ErrRouterPathPre                 = errors.New("web: 路由路径没有以 / 为开头")
+	ErrRouterPathSuf                 = errors.New("web: 路由路径不能以 / 为结尾")
+	ErrRouterPathContinuouslyEmpty   = errors.New("web: 路由路径出现了连续的 / ")
+	ErrRouterPathParameterConflicts  = errors.New("web: 路由路径出现了重复的参数")
+	ErrRouterPathStarInWrongPosition = errors.New("web: 路由路径中通配符位置异常")
 )
 
 type router struct {
@@ -20,10 +30,44 @@ func newRouter() *router {
 	return &router{trees: map[string]*node{}}
 }
 
+func checkRouterPath(path string) error {
+	if path == "" {
+		return ErrRouterPathEmpty
+	}
+	if path == "/" {
+		return nil
+	}
+	if !strings.HasPrefix(path, "/") {
+		return ErrRouterPathPre
+	}
+	if strings.HasSuffix(path, "/") {
+		return ErrRouterPathSuf
+	}
+	mp := map[string]struct{}{}
+	paths := strings.Split(path[1:], "/")
+	for id, p := range paths {
+		if p == "" {
+			return ErrRouterPathContinuouslyEmpty
+		} else {
+			if strings.HasPrefix(p, "*") {
+				if id != len(paths)-1 {
+					return ErrRouterPathStarInWrongPosition
+				}
+			} else if strings.HasPrefix(p, ":") {
+				if _, ok := mp[p]; ok {
+					return ErrRouterPathParameterConflicts
+				}
+				mp[p] = struct{}{}
+			}
+		}
+	}
+	return nil
+}
+
 func (r *router) addRouter(method string, pattern string, handleFunc HandleFunc) {
 	log.Printf("try add router %s - %s\n", method, pattern)
-	if pattern == "" {
-		panic("web: 路由不能为空")
+	if err := checkRouterPath(pattern); err != nil {
+		panic(err)
 	}
 	root, ok := r.trees[method]
 	if !ok {
@@ -33,63 +77,55 @@ func (r *router) addRouter(method string, pattern string, handleFunc HandleFunc)
 		r.trees[method] = root
 	}
 	if pattern == "/" {
-		if root.handleFunc != nil {
-			panic(fmt.Sprintf("web：路由冲突 - %s", pattern))
-		}
 		root.handleFunc = handleFunc
 		log.Printf("add router %s - %s success\n", method, pattern)
 		return
 	}
-
-	if !strings.HasPrefix(pattern, "/") {
-		panic("web: 路由必须由 / 开头")
-	}
-	if strings.HasSuffix(pattern, "/") {
-		panic("web: 路由不能以 / 结尾")
-	}
-
 	// 切割得到路由路径
 	parts := strings.Split(pattern[1:], "/")
 	for _, part := range parts {
-		if part == "" {
-			panic("web: 路径错误，出现了连续的 / ")
-		}
 		root, ok = root.addNode(part)
 		if !ok {
 			panic(fmt.Sprintf("web: 路由冲突 - %s", pattern))
 		}
-	}
-	if root.handleFunc != nil {
-		panic(fmt.Sprintf("web：路由冲突 - %s", pattern))
 	}
 	root.handleFunc = handleFunc
 	log.Printf("add router %s - %s success\n", method, pattern)
 }
 
 // 获取路由匹配对应处理函数
-func (r *router) getRouter(method string, pattern string) (*node, bool) {
+func (r *router) getRouter(method string, pattern string) (*node, map[string]string, bool) {
+	params := make(map[string]string)
 	// 对参数进行适量检验
 	if pattern == "" {
-		return nil, false
+		return nil, nil, false
 	}
 	// 获取前缀树根节点进行查找对应路由
 	root, ok := r.trees[method]
 	if !ok {
-		return nil, false
+		return nil, nil, false
 	}
 	if pattern == "/" {
-		return root, true
+		return root, params, root.handleFunc != nil
 	}
 	// 切割得到路由路径
 	parts := strings.Split(pattern[1:], "/")
 	for _, part := range parts {
 		if part == "" {
-			return nil, false
+			return nil, nil, false
 		}
 		root = root.getNode(part)
 		if root == nil {
-			return nil, false
+			return nil, nil, false
+		}
+		if strings.HasPrefix(root.part, ":") {
+			params[root.part[1:]] = part
+		}
+		if strings.HasPrefix(root.part, "*") {
+			index := strings.Index(pattern, part)
+			params[root.part[1:]] = pattern[index:]
+			return root, params, root.handleFunc != nil
 		}
 	}
-	return root, root.handleFunc != nil
+	return root, params, root.handleFunc != nil
 }
